2dab92690771add7894ca01e8358d04b7e76f9fc
Camilo018 <cristian.lopez04@uptc.edu.co>
2020-07-27 00:19:03 -0500

    Se agrego archivo para reiniciar remotamente el servicio

--- peticiones.sh
+++ peticiones.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 reqFunction(){
-   time_total=$(curl -o /dev/null -L -qs -w "%{time_total}" http://192.168.1.33:3200/)
+   time_total=$(curl -o /dev/null -L -qs -w "%{time_total}" http://192.168.0.119:3200/)
    time_required=0,0013
    echo "El tiemo total de respuesta es: $time_total"
    echo "El tiempo requerido de respuesta es: $time_required"
+   validationFunction
+}
 
-   if awk 'BEGIN{exit ARGV[1]>=ARGV[2]}' "$time_total" "$time_required"
+validationFunction(){
+   if awk 'BEGIN{exit ARGV[1]>=ARGV[2]}' "$time_total" "$time_required"s
    then
       echo 'Peticion realizada de forma exitosa ^^'
       exit 0
    else
       echo 'Tiempo de respuesta excedido, se procedera a tomar acciones en el servidor'
-      sshpass -p "millonarios018" ssh -o StrictHostKeyChecking=no kamil018@192.168.1.33
+      sshpass -p "12345" ssh -o StrictHostKeyChecking=no kamil018@192.168.0.119 'cd git_environment;cd Laboratorio-N-1;./reinicio.sh'
    fi
 }
+
 while true; do reqFunction & sleep 10; done
+
